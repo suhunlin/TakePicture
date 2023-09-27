3b6a5b4c7bc12e7a0d5cf518bf61554c3a920753
suhunlin <lin.suhun@msa.hinet.net>
2023-09-27 17:54:02 +0800

    Add layout and initial view

--- app/src/main/java/com/suhun/takepicture/MainActivity.java
+++ app/src/main/java/com/suhun/takepicture/MainActivity.java
@@ -3,12 +3,29 @@ package com.suhun.takepicture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        img = findViewById(R.id.lid_img);
+    }
+
+    public void takePic1Fun(View view){
+
+    }
+
+    public void takePic2Fun(View view){
+
     }
 }
