98677fbf6313472a6edb5780121d44b1c3381572
suhunlin <lin.suhun@msa.hinet.net>
2023-09-28 11:10:08 +0800

    Complete takePic2Fun

--- app/src/main/java/com/suhun/takepicture/MainActivity.java
+++ app/src/main/java/com/suhun/takepicture/MainActivity.java
@@ -10,20 +10,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private String tag = MainActivity.class.getSimpleName();
     private ImageView img;
+    private File saveDir;
     private ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -35,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
                         img.setImageBitmap(bitmap);
                     }
                 }
+            });
+    private ActivityResultLauncher<Intent> takePicResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                    if(result.getResultCode() == RESULT_OK){
+                        Bitmap bitmap = BitmapFactory.decodeFile(new File(saveDir + "/suhun.jpg").getAbsolutePath());
+                        img.setImageBitmap(bitmap);
+                    }
             });
 
     @Override
@@ -90,6 +104,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void takePic2Fun(View view){
-
+        saveDir = Environment.getExternalStoragePublicDirectory("Hiskio");
+        Uri uri = FileProvider.getUriForFile(this, getPackageName()+".fileprovider",
+                new File(saveDir + "/suhun.jpg"));
+        Log.d(tag, "-----Get uri from fileprovider" + uri + "-----");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        takePicResultLauncher.launch(intent);
     }
 }
